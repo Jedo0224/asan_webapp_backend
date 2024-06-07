9f30a4b82cffc1d5ccb0829f50bc3feafafffa5e
Jedo0224 <qkrwotjr0@gmail.com>
2024-06-07 16:14:10 +0900

    [feat] 비콘 device 등록, 조회 , 삭제, redis 저장 controller 추가

--- AsanVisualizationWebAppServer-main/src/main/java/com/asanhospital/server/controller/BeaconDataController.java
+++ AsanVisualizationWebAppServer-main/src/main/java/com/asanhospital/server/controller/BeaconDataController.java
@@ -5,16 +5,17 @@ import com.asanhospital.server.apiPayload.status.ErrorStatus;
 import com.asanhospital.server.domain.BeaconData;
 import com.asanhospital.server.dto.Mobile.BeaconDataRequest;
 import com.asanhospital.server.dto.Mobile.BeaconDataResponse;
+import com.asanhospital.server.dto.Mobile.BeaconDataDTO;
+import com.asanhospital.server.dto.Mobile.BeaconRegisterDTO;
 import com.asanhospital.server.service.Mobile.BeaconDataService;
 import com.asanhospital.server.repository.BeaconDataRepository;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -23,7 +24,6 @@ import java.util.List;
 public class BeaconDataController {
 
     private final BeaconDataService beaconDataService;
-    private final BeaconDataRepository beaconDataRepository;
 
     @PostMapping("/upload")
     public ResponseEntity<ApiResponse<BeaconDataResponse>> receiveBeaconData(@RequestBody List<BeaconDataRequest> beaconDataRequestList) {
@@ -31,7 +31,7 @@ public class BeaconDataController {
 
         try {
             for (BeaconDataRequest beaconDataRequest : beaconDataRequestList) {
-                beaconDataService.processBeaconData(beaconDataRequest);
+                beaconDataService.beaconCheckByRedis(beaconDataRequest);
             }
             BeaconDataResponse response = BeaconDataResponse.builder()
                     .status("success")
@@ -48,9 +48,58 @@ public class BeaconDataController {
         }
     }
 
-    @GetMapping("/all")
-    public ResponseEntity<ApiResponse<List<BeaconData>>> getAllBeaconData() {
-        List<BeaconData> beaconDataList = beaconDataRepository.findAll();
-        return ResponseEntity.ok(ApiResponse.onSuccessList(beaconDataList));
+    @GetMapping("/getDeviceLives")
+    public ResponseEntity<Map<String, Object>> getAllConnectedBeacon() {
+        Map<String, Object> beaconDataList = beaconDataService.getAllBeaconData();
+        return ResponseEntity.ok(beaconDataList);
+    }
+
+    @DeleteMapping("/deleteDevice")
+    public ResponseEntity<?> deleteDevice(@RequestBody List<BeaconDataDTO> beaconDataDTOs){
+
+        try {
+            for (BeaconDataDTO beaconList : beaconDataDTOs) {
+                beaconDataService.deleteBeaconData(beaconList);
+            }
+            BeaconDataResponse response = BeaconDataResponse.builder()
+                    .status("success")
+                    .message("Device delete successfully")
+                    .build();
+            return ResponseEntity.status(201).body(ApiResponse.onSuccess(response));
+        } catch (Exception e) {
+            log.error("Error processing request", e);
+            BeaconDataResponse errorResponse = BeaconDataResponse.builder()
+                    .status("error")
+                    .message("Failed to delete Device")
+                    .build();
+            return ResponseEntity.status(500).body(ApiResponse.onFailure(ErrorStatus._INTERNAL_SERVER_ERROR, errorResponse));
+        }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponse<BeaconDataResponse>> registerBeaconDevice(@RequestBody List<BeaconDataDTO> beaconDataDTOs ) {
+        try {
+            for (BeaconDataDTO beaconList : beaconDataDTOs) {
+                beaconDataService.saveBeaconData(beaconList);
+            }
+            BeaconDataResponse response = BeaconDataResponse.builder()
+                    .status("success")
+                    .message("Data processed successfully")
+                    .build();
+            return ResponseEntity.status(201).body(ApiResponse.onSuccess(response));
+        } catch (Exception e) {
+            log.error("Error processing request", e);
+            BeaconDataResponse errorResponse = BeaconDataResponse.builder()
+                    .status("error")
+                    .message("Failed to process data")
+                    .build();
+            return ResponseEntity.status(500).body(ApiResponse.onFailure(ErrorStatus._INTERNAL_SERVER_ERROR, errorResponse));
+        }
+    }
+
+    @GetMapping("/getDevices")
+    public ResponseEntity<List<BeaconDataDTO>> getAllBeaconDevice() {
+        List<BeaconDataDTO> beaconDataList = beaconDataService.getAllBeaconDevice();
+        return ResponseEntity.ok(beaconDataList);
     }
 }
