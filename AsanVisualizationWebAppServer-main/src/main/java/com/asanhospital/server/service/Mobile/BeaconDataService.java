3945921017b2a98b0a10af188dfe26ab9d182116
Jedo0224 <qkrwotjr0@gmail.com>
2024-06-07 16:15:29 +0900

    [feat] 비콘 device 등록, 조회 , 삭제, redis 저장 함수 interface에 추가

--- AsanVisualizationWebAppServer-main/src/main/java/com/asanhospital/server/service/Mobile/BeaconDataService.java
+++ AsanVisualizationWebAppServer-main/src/main/java/com/asanhospital/server/service/Mobile/BeaconDataService.java
@@ -2,7 +2,28 @@ package com.asanhospital.server.service.Mobile;
 
 import com.asanhospital.server.dto.Mobile.BeaconDataRequest;
 import com.asanhospital.server.dto.Mobile.BeaconDataResponse;
+import com.asanhospital.server.dto.Mobile.BeaconDataDTO;
+import com.asanhospital.server.dto.Mobile.BeaconRegisterDTO;
+
+import java.util.List;
+import java.util.Map;
+
 
 public interface BeaconDataService {
-    BeaconDataResponse processBeaconData(BeaconDataRequest beaconDataRequest);
+
+    Map<String, Object> getAllBeaconData();
+    void beaconCheckByRedis(BeaconDataRequest beaconDataRequest);
+//    BeaconDataResponse processBeaconData(BeaconDataRequest beaconDataRequest);
+
+    void saveBeaconData(BeaconDataDTO beaconList);
+
+    void deleteBeaconData(BeaconDataDTO beaconList);
+
+
+    List<BeaconDataDTO> getAllBeaconDevice();
+
+
+
+
+//    void deleteDevices(BeaconRegisterDTO beaconRegisterDTO);
 }
