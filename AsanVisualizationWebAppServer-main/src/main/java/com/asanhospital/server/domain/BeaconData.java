8dc4afc1bae0994c4cd996275026ffd5a4e7bd3d
Jedo0224 <qkrwotjr0@gmail.com>
2024-06-07 16:13:28 +0900

    [chore] id, deviceId, deviceName 등 연결 상태에 필요한 속성만 남김.

--- AsanVisualizationWebAppServer-main/src/main/java/com/asanhospital/server/domain/BeaconData.java
+++ AsanVisualizationWebAppServer-main/src/main/java/com/asanhospital/server/domain/BeaconData.java
@@ -1,23 +1,16 @@
 package com.asanhospital.server.domain;
 
+import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@Builder
 @Document(collection = "beacon_data")
 public class BeaconData {
     @Id
     private String id;
     private String deviceName;
     private String deviceAddress;
-    private byte[] manufacturerData;
-    private Integer temperature;
-    private Integer bleDataCount;
-    private String currentDateAndTime;
-    private String timestampNanos;
-    private Integer valueX;
-    private Integer valueY;
-    private Integer valueZ;
-    private Integer rating;
 }
