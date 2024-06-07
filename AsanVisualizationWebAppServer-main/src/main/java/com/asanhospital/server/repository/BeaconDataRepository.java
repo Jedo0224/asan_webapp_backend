134b73edc24f93528cc5ed489bfe9d76530c6b20
Jedo0224 <qkrwotjr0@gmail.com>
2024-06-07 16:14:48 +0900

    [feat] findByDeviceAddress 함수 추가 (비콘 device 등록시 중복 확인을 위함)

--- AsanVisualizationWebAppServer-main/src/main/java/com/asanhospital/server/repository/BeaconDataRepository.java
+++ AsanVisualizationWebAppServer-main/src/main/java/com/asanhospital/server/repository/BeaconDataRepository.java
@@ -4,7 +4,11 @@ import com.asanhospital.server.domain.BeaconData;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BeaconDataRepository extends MongoRepository<BeaconData, String> {
-    // Define custom query methods if needed
+    Optional<BeaconData> findByDeviceAddress(String deviceAddress);
+
+
 }
